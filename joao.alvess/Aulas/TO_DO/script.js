38e5730100bf082cf26f4d706db6164725f9b2b3
João Victor Guimarães Alves <joao.alvess@dinfor.uniceub>
2026-03-05 21:36:35 -0300

    Atividade to do

---
+++ joao.alvess/Aulas/TO_DO/script.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+
+    const input = this.document.getElementById("tarefaInput");
+    const btnAdicionar = this.document.getElementById("btnAdicionar");
+    const lista = this.document.getElementById("ListaTarefas")
+
+    btnAdicionar.addEventListener("click", () => {
+
+        const texto = input.value.trim();
+
+        //Criar um elemento da minha lista de tarefas
+        const li = this.document.createElement("Li");
+
+        //Adicionar o valor do input no meu item da lista
+        li.textContent = texto;
+
+        const btnRemover = this.document.createElement("button");
+        btnRemover.textContent = "x";
+        btnRemover.addEventListener("click", () => {
+            li.remove();
+        });
+
+        li.appendChild(btnRemover);
+
+
+        //Adiciona o item na lista para ser apresentado
+        lista.appendChild(li);
+
+        input.value = "";
+
+        
+    });
+};
